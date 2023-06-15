9f75f04a4de1f39bf45726846fa0fc2f6a7adb2f
KramarenkoVladyslav <93004336+KramarenkoVladyslav@users.noreply.github.com>
2023-06-15 14:30:30 +0300

    Add files via upload

---
+++ js/base.js
@@ -0,0 +1,29 @@
+console.log("Vladyslav");
+
+let firstName = "Vlad";
+let lastName = "Kramarenko";
+let studyGroup = "June";
+const birthOfYear = 2002;
+const isMarried = false;
+
+console.log(typeof birthOfYear, birthOfYear);
+console.log(typeof isMarried, isMarried);
+console.log(typeof firstName, firstName);
+
+let nallVariable = null;
+let undefinedVariable;
+
+console.log(typeof nallVariable, nallVariable);
+console.log(typeof undefinedVariable, undefinedVariable);
+
+prompt("Dear User your login is");
+prompt("Your login");
+prompt("Your password");
+
+let secondsInHour = 60 * 60;
+let secondsInDay = secondsInHour * 24;
+let secondsInMonth = secondsInDay * 30;
+
+console.log("Кількість секунд в годині: ", secondsInHour);
+console.log("Кількість секунд в добі: ", secondsInDay);
+console.log("Кількість секунд в місяці: ", secondsInMonth);
